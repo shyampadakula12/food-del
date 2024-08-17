e260ed6e6a6044e7acb85960420620b7b6b34085
shyampadakula12 <128414576+shyampadakula12@users.noreply.github.com>
2024-08-17 10:36:01 -0500

    Update StoreContext.jsx
    local host link changes

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000"; // Ensure the URL is correct
+  const url = "https://food-del-backend-wijf.onrender.com"; // Ensure the URL is correct
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
